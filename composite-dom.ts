f5008358a7d34b3df61044fa6df1a80155a39aac
Casey Lee <caseylee@D-173-250-234-116.dhcp4.washington.edu>
2017-01-25 11:19:03 -0800

    end of section

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,10 +2,14 @@ interface IDomElement {
   print();
 }
 
+// only prints tag 
+// calls dom elemnt if tag is not present 
 class DomElement implements IDomElement {
    
+   // print + DomElement + inner component of list 
 }
 
+// prints HI, I am a textnode being printed 
 class TextNode extends DomElement {
 
 }
